bb36f925850b728fa87a7422deea8bca4d7afc68
282Haniwa <282haniwa@gmail.com>
2020-12-25 16:26:40 +0900

    Emotionの設定

--- src/components/Hello/Hello.tsx
+++ src/components/Hello/Hello.tsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { css } from '@emotion/react'
+
+const hello = css`
+  color: red;
+`
 
 type Props = {
   name: string
@@ -6,5 +11,9 @@ type Props = {
 
 export const Hello: React.FC<Props> = (props) => {
   const { name } = props
-  return <div>Hello {name}!</div>
+  return (
+    <div>
+      <h1 css={hello}>Hello {name}!</h1>
+    </div>
+  )
 }
